bdafd44c85b5f3b97070e24bb6fc34868ab05952
Ilia-sever <terebenin_il@mail.ru>
2017-02-07 00:20:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,29 @@
-<html>
-<head></head>
-<body onload="f1()">
-<script>
-function f1() {
-	let arr =[
-	user = {f:'Васин',i:'Вася',o:'Васильевич'},
-	buser = {f2: 'Петин',i2:'Петя'},
-	muser = {g:'Васин',i:'Вася',o:'Васильевич', ret:456}
-	];
-	let f2 = (arr) => 
-	{
-	var el;
-	let arr2 = new Array (arr.length);
+let f = (arr) => 
+{
+	function callback(call) {
+		if (typeof(call)=='function') call()
+	};
+	let el;
+	let arr2  = new Array (arr.length);
 	for (let i=0;i<arr2.length;i++)
 	{
 		el=0;
-		for (let j in arr[i])
-			el++;
+		for (let j in arr[i]) el++;
 		arr2[i]=el; 
 	}
 	let max=0;
 	for (let i=0;i<arr2.length;i++)
 		if (arr2[max]<arr2[i]) max=i;
-	alert(max);
-	}
-	f2(arr);
-
+	for (let i in arr[arr.length-1])
+		callback(arr[arr.length-1][i])   
+	return arr[max];
 }
-</script>
-</body>
-</html>
+
+	/* готовый объект в помощь
+	let arr =[
+		user = {f:'Васин',i:'Вася',o:'Васильевич'},
+		buser = {f2: 'Петин',i2:'Петя'},
+		muser = {g:'Васин',i:'Вася',o:'Васильевич', ret:456},
+		iser = {g:'sdf',r:'345',tth: function () {alert('Wat')}}
+	];
+	*/
